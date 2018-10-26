6646940f15770ceabb7b9841ee2b2db568246e85
Alex Sheehan <alexsheehan@gmail.com>
2018-10-25 19:51:51 -0700

    Set up server and database seed

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3001;
+const mongoose = require('mongoose');
+const db = require('../database/index.js');
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+});
+
+app.listen(port, () => {
+  console.log(`server running at: http://localhost:${port}`);
+});
